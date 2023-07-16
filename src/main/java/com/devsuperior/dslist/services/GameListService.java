f394c19287bae7177ae48a4f86f599d9da3c3e14
João Matheus Lamão <joaomatheuslamao9@gmail.com>
2023-07-16 15:38:22 -0300

    Feat: endpoint referente à movimentação na lista

--- src/main/java/com/devsuperior/dslist/services/GameListService.java
+++ src/main/java/com/devsuperior/dslist/services/GameListService.java
@@ -5,6 +5,7 @@ import com.devsuperior.dslist.dto.GameListDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.entities.GameList;
+import com.devsuperior.dslist.projection.GameMinProjection;
 import com.devsuperior.dslist.repositories.GameListRepository;
 import com.devsuperior.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +19,9 @@ public class GameListService {
 	
 	@Autowired
 	private GameListRepository gameListRepository;
+
+	@Autowired
+	private GameRepository gameRepository;
 	
 	//manipula um objeto GameList para retornar um objeto do tipo GameListDTO
 	@Transactional(readOnly = true)
@@ -27,5 +31,17 @@ public class GameListService {
 		return gameListDTO;
 	}
 
+
+	@Transactional(readOnly = true)
+	public void move(Long listId, int sourceIndex, int destinationIndex){
+		List<GameMinProjection> result = gameRepository.searchByList(listId);
+
+		GameMinProjection obj = result.remove(sourceIndex);
+		result.add(destinationIndex, obj);
+
+		for (int i = 0; i < result.size(); i++){
+			gameListRepository.updateBelongingPosition(listId, result.get(i).getId(), i);
+		}
+	}
 
 }
